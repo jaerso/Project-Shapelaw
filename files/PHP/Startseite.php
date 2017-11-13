37d95327b71c04c5d161c3a53865eb6613204d3c
jaerso <28540339+jaerso@users.noreply.github.com>
2017-11-13 15:51:46 +0100

    bootstrap hinzugefügt + anpassungen

---
+++ files/PHP/Startseite.php
@@ -0,0 +1,25 @@
+<section id="home">
+<div class="home-center">
+    <h1>WILLKOMMEN</h1>
+    <h2>auf Shapelaw</h2>
+  </div>
+
+    <a href="#about">
+        <i class="fa fa-angle-double-down fa-4x"></i>
+      </a>
+  </section>
+
+  <section id="about">
+      <div class="container">
+      <h1>ÜBER UNS</h1>
+      <hr>
+
+      <h2><strong>Was kannst du hier machen?</strong></h2>
+      <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy 
+          eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam 
+          voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet 
+          clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+      </p>
+
+    </div>
+    </section>
