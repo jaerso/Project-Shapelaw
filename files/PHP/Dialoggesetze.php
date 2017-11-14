1fd2962bdac369379b2d86e3ac749e35ef052f11
jaerso <28540339+jaerso@users.noreply.github.com>
2017-11-14 13:24:25 +0100

    Dialogseite angepasst

--- files/PHP/Dialoggesetze.php
+++ files/PHP/Dialoggesetze.php
@@ -1,6 +1,6 @@
-
-<section id="Gestaltgesetze1">
 <div class="container">
+<div id="Dialoggestalltung">
+
     
 <h1>Dialoggestalltung 1</h1>
 
@@ -10,10 +10,8 @@
     Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
     </p>
     </div>
-</section>
 
-<section id="Gestaltgesetze2">
-<div class="container">
+<div id="Dialoggestalltung">
 
 <h1>Dialoggestalltung 2</h1>
 
@@ -23,5 +21,5 @@
     Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
     </p>
     </div>
-</section>
+</div>
 
