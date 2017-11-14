d92b4463c288dacb121387b0503059ae68db4499
JHoeppe <jhoeppe@users.noreply.github.com>
2017-11-14 14:22:23 +0100

    css

--- files/PHP/Gestaltgesetze.php
+++ files/PHP/Gestaltgesetze.php
@@ -1,59 +1,36 @@
-<<<<<<< HEAD
-<div class ="container" id = "Seitenüberschrift">
-    <h1> Gestaltgesetze </h1>
-</div>
-
-<div class = "container">
-
-    <h1> Gesetz der Ähnlichkeit / Gleichheit </h1>
-=======
 <div class="container">
-<h2> Gestaltgesetze </h2>
+    
+    <h2> Gestaltgesetze </h2>
 
-<div id="Gestaltgesetze">
+    <div id="Gestaltgesetze">
 
-    <h3> Gesetz der Ähnlichkeit / Gleichheit </h3>
->>>>>>> 90fabc46c78b7436ce880fc78c9ce6f41b11256d
+        <h1> Gesetz der Ähnlichkeit / Gleichheit </h1>
 
-    <p>Elemente, welche einander gleichen, nehmen wir als zusammengehörig wahr. 
-    Dabei vergleichen wir die optischen Reize bei verschiedenfarbigen Elementen,
-    unterscheiden zwischen Helligkeiten, Größe der Elemente oder ihrer Orientierung aber auch ihrer Form. 
-    So erkennen Menschen in kürzester Zeit verschiedene Formen in einer Ansammlung an bekannten geometrischen Figuren und können diese im Kopf nach Kreis,
-    Quadrat, Dreieck etc. problemlos gruppieren. </p>
+        <p>Elemente, welche einander gleichen, nehmen wir als zusammengehörig wahr. 
+        Dabei vergleichen wir die optischen Reize bei verschiedenfarbigen Elementen,
+        unterscheiden zwischen Helligkeiten, Größe der Elemente oder ihrer Orientierung aber auch ihrer Form. 
+        So erkennen Menschen in kürzester Zeit verschiedene Formen in einer Ansammlung an bekannten geometrischen Figuren und können diese im Kopf nach Kreis,
+        Quadrat, Dreieck etc. problemlos gruppieren. </p>
 
-    <img src="files\img\GesetzDerÄhnlichkeitGleichheit.jpg" max-width: 100%; height: auto;>
+        <img src="files\img\GesetzDerÄhnlichkeitGleichheit.jpg" max-width: 100%; height: auto;>
 
-    <p>Angewendet wird es zum Gesetz der Gleichheit. Hierbei wird z.B. bei Formularen in Computeranwendungen die gleiche Sorte von Eingabefeldern benutzt. 
-    So erkennt der Nutzer sofort, dass alle Rechtecke Eingabefelder sind. </p>
+        <p>Angewendet wird es zum Gesetz der Gleichheit. Hierbei wird z.B. bei Formularen in Computeranwendungen die gleiche Sorte von Eingabefeldern benutzt. 
+        So erkennt der Nutzer sofort, dass alle Rechtecke Eingabefelder sind. </p>
 
-    <img src="">
+        <img src="">
 
-    <p>Soll innerhalb der gleichen Formen ein Unterschied bei den Eingabefeldern dargestellt werden, so kann das mit dem Aspekt der Farbe erreicht werden. 
-    Dadurch weiß der Nutzer, es handelt sich immer noch um Eingabefelder, die jedoch zu einer anderen Gruppe gehören als die weißen Felder. </p>
+        <p>Soll innerhalb der gleichen Formen ein Unterschied bei den Eingabefeldern dargestellt werden, so kann das mit dem Aspekt der Farbe erreicht werden. 
+        Dadurch weiß der Nutzer, es handelt sich immer noch um Eingabefelder, die jedoch zu einer anderen Gruppe gehören als die weißen Felder. </p>
 
-    <img src="">
+        <img src="">
 
-    <p>Gleiches gilt für Buttons auf Webseiten. Durch ihre ähnliche Form und evtl. auch farbliche Absetzung fasst der Nutzer diese Buttons einer funktionalen Gruppe zusammen. </p>
+        <p>Gleiches gilt für Buttons auf Webseiten. Durch ihre ähnliche Form und evtl. auch farbliche Absetzung fasst der Nutzer diese Buttons einer funktionalen Gruppe zusammen. </p>
 
-    <p>Das Gesetz der Ähnlichkeit wird verstärkt durch das Gesetz der Nähe.</p>
-<<<<<<< HEAD
+        <p>Das Gesetz der Ähnlichkeit wird verstärkt durch das Gesetz der Nähe.</p>
+    </div>
 
-</div>
+    <div id="Gestaltgesetze">
 
+    </div>
 
-
-<div class ="container">
-
-    <h1> </h2>
-=======
-</div>
-
-<div id="Gestaltgesetze">
->>>>>>> 90fabc46c78b7436ce880fc78c9ce6f41b11256d
-
-</div>
-
-
-
-</section>
 </div>
