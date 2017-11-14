d097d4246ddb5c1a3ca1f71c095e8b1d1dfd2424
JHoeppe <jhoeppe@users.noreply.github.com>
2017-11-14 19:47:11 +0100

    css

--- files/PHP/Gestaltgesetze.php
+++ files/PHP/Gestaltgesetze.php
@@ -1,6 +1,4 @@
 <div class="container">
-    
-    <h2> Gestaltgesetze </h2>
 
     <div id="Gestaltgesetze">
 
@@ -14,21 +12,28 @@
         So erkennen Menschen in kürzester Zeit verschiedene Formen in einer Ansammlung an bekannten geometrischen Figuren und können diese im Kopf nach Kreis,
         Quadrat, Dreieck etc. problemlos gruppieren. </p>
 
-        <img src="files\img\GesetzDerÄhnlichkeitGleichheit.jpg" max-width: 100%; height: auto;>
+        
+        <img class = "content-img" src="files\img\GesetzDerÄhnlichkeitGleichheit.jpg" >
+        
+        <br>
 
         <p>Angewendet wird es zum Gesetz der Gleichheit. Hierbei wird z.B. bei Formularen in Computeranwendungen die gleiche Sorte von Eingabefeldern benutzt. 
         So erkennt der Nutzer sofort, dass alle Rechtecke Eingabefelder sind. </p>
 
         <img src="">
 
+        <br>
+
         <p>Soll innerhalb der gleichen Formen ein Unterschied bei den Eingabefeldern dargestellt werden, so kann das mit dem Aspekt der Farbe erreicht werden. 
         Dadurch weiß der Nutzer, es handelt sich immer noch um Eingabefelder, die jedoch zu einer anderen Gruppe gehören als die weißen Felder. </p>
+        
 
         <img src="">
 
         <p>Gleiches gilt für Buttons auf Webseiten. Durch ihre ähnliche Form und evtl. auch farbliche Absetzung fasst der Nutzer diese Buttons einer funktionalen Gruppe zusammen. </p>
 
         <p>Das Gesetz der Ähnlichkeit wird verstärkt durch das Gesetz der Nähe.</p>
+        <br>
     </div>
 
     <div id="Gestaltgesetze">
