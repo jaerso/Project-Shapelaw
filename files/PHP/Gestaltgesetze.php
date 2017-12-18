8164d7ed2feb85dd2a9309fab0668233649e2b31
jaerso <28540339+jaerso@users.noreply.github.com>
2017-12-18 16:46:47 +0100

    übersichtsseite angepasst

--- files/PHP/Gestaltgesetze.php
+++ files/PHP/Gestaltgesetze.php
@@ -29,50 +29,50 @@
 
                     <div class="row">
                         <div class="col-sm-4">
-                            <div id='divBorder' style="cursor: pointer;">
+                            <div class="divBorder" style="cursor: pointer;">
                                 <a href="index.php?page=appearance&gesetz=1">
-                                    <h1 style="text-align: center; margin-bottom: 1em !important;"> Gesetz der Ähnlichkeit</h1>
+                                    <h1> Gesetz der Ähnlichkeit</h1>
                                     <!-- <img class= "galerie-Bilder" src="files/minigolfbahnen/bahn1.jpg" alt="Gesetz der Ähnlichkeit / Gleichheit"> -->
                                 </a>
                             </div>
                         </div>
                         <div class="col-sm-4">
-                            <div id='divBorder' style="cursor: pointer;">
+                            <div class="divBorder" style="cursor: pointer;">
                                 <a href="index.php?page=appearance&gesetz=2">
-                                    <h1 style="text-align: center; margin-bottom: 1em !important;"> Gesetz der <br>Nähe</h1>
+                                    <h1> Gesetz der <br> Nähe </h1>
                                     <!-- <img class= "galerie-Bilder" src="files/minigolfbahnen/bahn2.jpg" alt="Bildbeschreibung Platzhalter"> -->
                                 </a>
                             </div>
                         </div>
                         <div class="col-sm-4">
-                            <div id='divBorder' style="cursor: pointer;">
+                            <div class="divBorder" style="cursor: pointer;">
                                 <a href="index.php?page=appearance&gesetz=3">
-                                    <h1 style="text-align: center; margin-bottom: 1em !important;"> Gesetz der guten Fortsetzung</h1>
+                                    <h1> Gesetz der guten Fortsetzung</h1>
                                     <!-- <img class= "galerie-Bilder" src="files/minigolfbahnen/bahn3.jpg" alt="Bildbeschreibung Platzhalter"> -->
                                 </a>
                             </div>
                         </div>
                         <div class="col-sm-4">
-                            <div id='divBorder' style="cursor: pointer;">
+                            <div class="divBorder" style="cursor: pointer;">
                                 <a href="index.php?page=appearance&gesetz=4">
-                                    <h1 style="text-align: center; margin-bottom: 1em !important;"> Gesetz der Schließung</h1>
+                                    <h1> Gesetz der Schließung</h1>
                                     <!-- <img class= "galerie-Bilder" src="files/minigolfbahnen/bahn4.jpg" alt="Bildbeschreibung Platzhalter"> -->
                                 </a>
                             </div>
                         </div>
 
                         <div class="col-sm-4">
-                            <div id='divBorder' style="cursor: pointer;">
+                            <div class="divBorder" style="cursor: pointer;">
                                 <a href="index.php?page=appearance&gesetz=5">
-                                    <h1 style="text-align: center; margin-bottom: 1em !important;"> Gesetz der Symmetrie</h1>
+                                    <h1> Gesetz der Symmetrie</h1>
                                     <!-- <img class= "galerie-Bilder" src="files/minigolfbahnen/bahn5.jpg" alt="Bildbeschreibung Platzhalter"> -->
                                 </a>
                             </div>
                         </div>
                         <div class="col-sm-4">
-                            <div id='divBorder' style="cursor: pointer;">
+                            <div class="divBorder" style="cursor: pointer;">
                                 <a href="index.php?page=appearance&gesetz=6">
-                                    <h1 style="text-align: center; margin-bottom: 1em !important;"> Gesetz der guten Gestalt</h1>
+                                    <h1> Gesetz der guten Gestalt</h1>
                                     <!-- <img class= "galerie-Bilder" src="files/minigolfbahnen/bahn6.jpg" alt="Bildbeschreibung Platzhalter"> -->
                                 </a>
                             </div>
