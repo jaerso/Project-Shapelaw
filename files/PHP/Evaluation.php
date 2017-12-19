1b5c79a18b8dc117591f991a9dc757c7b2d7f213
DominikSommer <29626701+DominikSommer@users.noreply.github.com>
2017-12-19 15:46:53 +0100

    Punkteausgabe

--- files/PHP/Evaluation.php
+++ files/PHP/Evaluation.php
@@ -8,17 +8,10 @@ include_once 'files/includes/evaluation.inc.php';
 
 $result=Endergebnis();
 //print_r($result);
+$size=$_SESSION['size'];
 session_unset();
 session_destroy();
-?>
-<!--<script>
-var result=<?php //echo "$result";
-?>
-document.getElementById("demo").innerHTML = result;
-</script>
 
-<div id=demo></div>-->
-<?php
 echo"<div>";
 /*if($result>=50){
 echo"<h1><strong>Glückwunsch!</strong><br> Sie haben $result %!</h1>";
@@ -27,8 +20,11 @@ elseif($result<50){
   echo"<h1><strong>Schade!</strong><br> Sie haben $result %! Vielleicht beim nächsten mal</h1>";
 }
     */
-    echo"<h1>Sie haben $result Punkte!</h1>";
-    echo "</div>"
+
+    
+      echo"<h1>Sie haben $result von $size Punkten!</h1>";
+      echo "</div>"
+    
   ?>
 
   <a href="index.php?page=home">Zurück zur Startseite!</a>
